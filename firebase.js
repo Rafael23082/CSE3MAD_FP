05dd862acaaa84fde51faac56be7347c38867bd9
Rafael Anderson <raf.anderson777@gmail.com>
2026-06-14 15:45:41 +0700

    hide firebase secrets

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDipQOWsouhl5bJPBN71d21ozvy_yh2x7s",
-  authDomain: "se3mad-fp-2e58f.firebaseapp.com",
-  projectId: "cse3mad-fp-2e58f",
-  storageBucket: "se3mad-fp-2e58f.firebasestorage.app",
-  messagingSenderId: 435684681786,
-  appId: "1:435684681786:web:fad73df6a456484226ed03",
-  measurementId: "G-SJGVHEZBQ0",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
